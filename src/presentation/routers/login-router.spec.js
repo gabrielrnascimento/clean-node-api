f8af98f5e8c3f142d91da798a34c0a7b808d7d5b
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2022-09-22 20:51:55 -0300

    ensure LoginRouter calls EmailValidator with correct email

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -7,19 +7,20 @@ const InvalidParamError = require('../helpers/invalid-param-error')
 // Degign Pattern - Factory
 const makeSut = () => {
   const authUseCaseSpy = makeAuthUseCase()
-  const emailVaidatorSpy = makeEmailValidator()
+  const emailValidatorSpy = makeEmailValidator()
   authUseCaseSpy.accessToken = 'valid_token'
-  const sut = new LoginRouter(authUseCaseSpy, emailVaidatorSpy) // Dependecy Injection
+  const sut = new LoginRouter(authUseCaseSpy, emailValidatorSpy) // Dependecy Injection
   return {
     sut,
     authUseCaseSpy,
-    emailVaidatorSpy
+    emailValidatorSpy
   }
 }
 
 const makeEmailValidator = () => {
   class EmailValidatorSpy { // Mock object (Spy)
     isValid (email) {
+      this.email = email
       return this.isEmailValid
     }
   }
@@ -180,8 +181,8 @@ describe('Login Router', () => {
   })
 
   test('"Should return 400 if no an invalid email is provided', async () => {
-    const { sut, emailVaidatorSpy } = makeSut()
-    emailVaidatorSpy.isEmailValid = false
+    const { sut, emailValidatorSpy } = makeSut()
+    emailValidatorSpy.isEmailValid = false
     const httpRequest = {
       body: {
         email: 'invalid_email@mail.com',
@@ -236,5 +237,17 @@ describe('Login Router', () => {
     }
     const httpResponse = await sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
+  })
+
+  test('"Should call EmailValidator with correct email', async () => {
+    const { sut, emailValidatorSpy } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
+    await sut.route(httpRequest)
+    expect(emailValidatorSpy.email).toBe(httpRequest.body.email)
   })
 })
