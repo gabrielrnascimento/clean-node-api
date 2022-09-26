537510bd8091c3c4fdc4b69ea7492714892327a0
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2022-09-26 10:37:16 -0300

    remove unnecessary if statements

--- src/domain/usecases/auth-usecase.js
+++ src/domain/usecases/auth-usecase.js
@@ -1,4 +1,4 @@
-const { MissingParamError, InvalidParamError } = require('../../utils/errors')
+const { MissingParamError } = require('../../utils/errors')
 
 module.exports = class AuthUseCase {
   constructor (loadUserByEmailRepository) {
@@ -8,12 +8,9 @@ module.exports = class AuthUseCase {
   async auth (email, password) {
     if (!email) {
       throw new MissingParamError('email')
-    } if (!password) {
+    }
+    if (!password) {
       throw new MissingParamError('password')
-    } if (!this.loadUserByEmailRepository) {
-      throw new MissingParamError('loadUserByEmailRepository')
-    } if (!this.loadUserByEmailRepository.load) {
-      throw new InvalidParamError('loadUserByEmailRepository')
     }
     const user = await this.loadUserByEmailRepository.load(email)
     if (!user) {
