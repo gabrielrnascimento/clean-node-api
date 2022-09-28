4773bcdb25990fc4298ed5215331645ea9bc644d
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2022-09-28 09:43:18 -0300

    ensure Encrypter calls bcrypt with correct values

--- src/utils/helpers/encrypter.spec.js
+++ src/utils/helpers/encrypter.spec.js
@@ -13,13 +13,18 @@ describe('Encrypter', () => {
     const isValid = await sut.compare('any_value', 'hashed_value')
     expect(isValid).toBe(true)
   })
-})
 
-describe('Encrypter', () => {
   test('Should return false if bcrypt returns false', async () => {
     const sut = new Encrypter()
     bcrypt.isValid = false
     const isValid = await sut.compare('any_value', 'hashed_value')
     expect(isValid).toBe(false)
+  })
+
+  test('Should call bcrypt with correct values', async () => {
+    const sut = new Encrypter()
+    await sut.compare('any_value', 'hashed_value')
+    expect(bcrypt.value).toBe('any_value')
+    expect(bcrypt.hash).toBe('hashed_value')
   })
 })
