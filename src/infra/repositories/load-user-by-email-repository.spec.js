29fdc6ddce0f19e233b064d367d285c03609f9a0
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2022-09-29 13:11:05 -0300

    ensure LoadUserByEmailRepository returns an user id

--- src/infra/repositories/load-user-by-email-repository.spec.js
+++ src/infra/repositories/load-user-by-email-repository.spec.js
@@ -47,10 +47,14 @@ describe('LoadUserByEmail Repository', () => {
 
   test('Should return an user if user is found', async () => {
     const { sut, userModel } = makeSut()
-    await userModel.insertOne({
-      email: 'valid_email@mail.com'
+    const fakeUser = await userModel.insertOne({
+      email: 'valid_email@mail.com',
+      name: 'any_name',
+      age: 50,
+      state: 'any_state',
+      password: 'hashed_password'
     })
     const user = await sut.load('valid_email@mail.com')
-    expect(user.email).toBe('valid_email@mail.com')
+    expect(user._id).toEqual(fakeUser.insertedId)
   })
 })
